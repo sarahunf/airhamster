4323e01564dba777b65f0f6931f67b9f358b5e1d
Sarah Reis <reis.sarahtbr@gmail.com>
2022-01-11 23:17:16 -0300

    player acceleration

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+namespace Player
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        private Rigidbody2D rb;
+        [SerializeField] private float acceleration;
+        [SerializeField] private float maxSpeed;
+        [SerializeField] private float speed;
+        private float startingSpeed;
+        [SerializeField] private float moveX;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            startingSpeed = speed;
+        }
+
+        private void Update()
+        {
+            if (Input.GetAxis("Horizontal") != 0)
+            {
+                if (speed > maxSpeed)
+                    speed = maxSpeed;
+                else
+                    speed += acceleration * Time.deltaTime;
+
+                var translation = Time.deltaTime * speed;
+                moveX = Input.GetAxis("Horizontal") * translation;
+            }
+            else
+            {
+                if (speed < startingSpeed)
+                    speed = startingSpeed;
+                else
+                    speed -= acceleration * Time.deltaTime;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            Vector2 velocity = rb.velocity;
+            velocity.x = moveX;
+            rb.velocity = velocity;
+        }
+    }
+}
